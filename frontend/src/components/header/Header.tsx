a765fb48bdea34a77389cde552b90b67bc4527f9
Matis121 <mateusz6246@gmail.com>
2024-08-09 21:59:34 +0200

    style: hidden logo on mobile app version

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -32,48 +32,50 @@ const Header = () => {
     <header
       className={`fixed flex z-20 w-full items-center border-b py-2 bg-white bg-opacity-95 dark:bg-neutral-950 dark:border-neutral-800 overflow-hidden ${isMobile ? "px-4" : "px-10"}`}
     >
-      <Link to="/">
-        <span className=" text-blue-400 font-semibold text-xl">
-          ShopPlanner
-        </span>
-      </Link>
       {!isMobile ? (
-        <div className="flex ml-24 gap-8">
-          <Link
-            to="/mylists"
-            className="relative font-medium text-md p-2 flex flex-row items-center space-x-2 rounded-md duration-200 hover:bg-neutral-100 text-neutral-600 dark:hover:bg-neutral-800 dark:text-neutral-500 dark:hover:text-white"
-          >
-            {({ isActive }) => (
-              <div
-                className={`flex space-x-2 ${isActive && "dark:text-white text-blue-400"}`}
-              >
-                <LuListChecks size={20} />
-                <span className="text-md">My lists</span>
-                {isActive && isActiveElement()}
-              </div>
-            )}
+        <>
+          <Link to="/">
+            <span className=" text-blue-400 font-semibold text-xl">
+              ShopPlanner
+            </span>
           </Link>
-          <Link
-            to="/groups"
-            className="relative font-medium text-md p-2 flex flex-row items-center space-x-2 rounded-md duration-200 hover:bg-neutral-100 text-neutral-600 dark:hover:bg-neutral-800 dark:text-neutral-500 dark:hover:text-white"
-          >
-            {({ isActive }) => (
-              <div
-                className={`flex space-x-2 ${isActive && "dark:text-white text-blue-400"}`}
-              >
-                <LuUsers size={20} />
-                <span className="text-md">Groups</span>
-                {isActive && isActiveElement()}
-              </div>
-            )}
-          </Link>
-        </div>
+          <div className="flex ml-24 gap-8">
+            <Link
+              to="/mylists"
+              className="relative font-medium text-md p-2 flex flex-row items-center space-x-2 rounded-md duration-200 hover:bg-neutral-100 text-neutral-600 dark:hover:bg-neutral-800 dark:text-neutral-500 dark:hover:text-white"
+            >
+              {({ isActive }) => (
+                <div
+                  className={`flex space-x-2 ${isActive && "dark:text-white text-blue-400"}`}
+                >
+                  <LuListChecks size={20} />
+                  <span className="text-md">My lists</span>
+                  {isActive && isActiveElement()}
+                </div>
+              )}
+            </Link>
+            <Link
+              to="/groups"
+              className="relative font-medium text-md p-2 flex flex-row items-center space-x-2 rounded-md duration-200 hover:bg-neutral-100 text-neutral-600 dark:hover:bg-neutral-800 dark:text-neutral-500 dark:hover:text-white"
+            >
+              {({ isActive }) => (
+                <div
+                  className={`flex space-x-2 ${isActive && "dark:text-white text-blue-400"}`}
+                >
+                  <LuUsers size={20} />
+                  <span className="text-md">Groups</span>
+                  {isActive && isActiveElement()}
+                </div>
+              )}
+            </Link>
+          </div>
+        </>
       ) : (
-        <div className="ml-4 flex">
+        <div className="flex">
           <MobileHamburger />
         </div>
       )}
-      <div className="flex items-center ml-auto gap-4">
+      <div className="flex items-center ml-auto gap-3">
         <Notifications />
         <ToggleTheme />
         <DropdownMenu>
